4f8c5125cd6d13762854065c0ce9e5745d4a6433
begimov <begimov@gmail.com>
2017-05-23 14:43:32 +0300

    url to admin panel changed

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -7,7 +7,7 @@
       </div>
       <div class="col-md-6 col-xs-6">
         <p class="text-footer">Больше клиентов для вашего бизнеса:</p>
-        <a class="btn btn-primary btn-xs" href="http://46.173.214.171:8080/" role="button">Добавить акцию</a>
+        <a class="btn btn-primary btn-xs" href="https://admin.onsells.ru" role="button">Добавить акцию</a>
       </div>
     </div>
   </div>
