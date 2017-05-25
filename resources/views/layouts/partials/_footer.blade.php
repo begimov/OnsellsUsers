6f4872b8004a25e427fdc82d399b9e5dd31c9beb
begimov <begimov@gmail.com>
2017-05-25 11:11:51 +0300

    social icons added and footer modified

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -2,12 +2,19 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6 col-xs-6">
-        <p class="text-footer">Контакты:</p>
-        <p class="text-footer">support@onsells.ru<br>8 (921) 999 99 99</p>
+          <ul class="list-inline">
+            <li class="text-footer"><a href="https://vk.com/onsells"><img src="{{ asset('img/footer/vk.png') }}"></a></li>
+            <li><a href="https://www.instagram.com/onsells.ru"><img src="{{ asset('img/footer/instagram.png') }}"></a></li>
+            <li><a href="https://www.facebook.com/onsells.ru"><img src="{{ asset('img/footer/facebook.png') }}"></a></li>
+            <li><a href="https://telegram.me/nikitinskiy"><img src="{{ asset('img/footer/telegram.png') }}"></a></li>
+            <li><a href="mailto:hello@onsells.ru" class="btn btn-default btn-xs">Напишите нам</a></li>
+          </ul>
       </div>
       <div class="col-md-6 col-xs-6">
-        <p class="text-footer">Больше клиентов для вашего бизнеса:</p>
-        <a class="btn btn-primary btn-xs" href="https://admin.onsells.ru" role="button">Добавить акцию</a>
+        <ul class="list-inline">
+          <li class="text-footer">Больше клиентов для вашего бизнеса:</li>
+          <li><a class="btn btn-primary btn-xs" href="https://admin.onsells.ru" role="button">Добавить акцию</a></li>
+        </ul>
       </div>
     </div>
   </div>
