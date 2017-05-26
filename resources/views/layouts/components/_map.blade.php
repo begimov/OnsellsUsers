13b0597aac204df33ab87f3e41d48bf3449ab250
begimov <begimov@gmail.com>
2017-05-26 14:48:36 +0300

    google maps geolocation added

--- resources/views/layouts/components/_map.blade.php
+++ resources/views/layouts/components/_map.blade.php
@@ -10,47 +10,46 @@
 
 <script type="text/javascript">
 
-function initMap() {
-  // TODO: replace by geolocation data
-  var defaultCenterLat = 59.9307772;
-  var defaultCenterLng = 30.3276762;
-  var defaultSearchRadius = 3000;
-  var markersArray = [];
+  function initMap() {
+    var defaultCenterLat = 59.9307772;
+    var defaultCenterLng = 30.3276762;
+    var defaultSearchRadius = 5000;
+    var markersArray = [];
 
-  var mapOptions = {
-    zoom: 13,
-    center: {
-      lat: defaultCenterLat,
-      lng: defaultCenterLng
-    },
-    styles: [
-      {
-        "featureType": "poi",
-        "stylers": [
-          { "visibility": "off" }
-        ]
-      }
-    ],
-    mapTypeControl: false,
-    streetViewControl: false,
-  }
+    var mapOptions = {
+      zoom: 12,
+      center: {
+        lat: defaultCenterLat,
+        lng: defaultCenterLng
+      },
+      styles: [
+        {
+          "featureType": "poi",
+          "stylers": [
+            { "visibility": "off" }
+          ]
+        }
+      ],
+      mapTypeControl: false,
+      streetViewControl: false,
+    }
 
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-  var infoWindow = new google.maps.InfoWindow();
+    var infoWindow = new google.maps.InfoWindow();
 
-  var searchCircle = new google.maps.Circle({
-      strokeColor: '#2ea1a6',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#2ea1a6',
-      fillOpacity: 0.1,
-      map: map,
-      center: map.getCenter(),
-      draggable: false,
-      editable: true,
-      radius: defaultSearchRadius
-    });
+    var searchCircle = new google.maps.Circle({
+        strokeColor: '#2ea1a6',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#2ea1a6',
+        fillOpacity: 0.1,
+        map: map,
+        center: map.getCenter(),
+        draggable: false,
+        editable: true,
+        radius: defaultSearchRadius
+      });
 
     google.maps.event.addListener(searchCircle, 'radius_changed', function() {
       getData(searchCircle.getCenter().lat(), searchCircle.getCenter().lng(), searchCircle.getRadius());
@@ -61,73 +60,69 @@ function initMap() {
 
     getData(defaultCenterLat, defaultCenterLng, defaultSearchRadius);
 
-  function getData(lat, lng, radius) {
-    $.ajax({
-      url: 'webapi/locations',
-      type: 'get',
-      dataType: 'json',
-      data: {
-        lat: lat,
-        lng: lng,
-        radius: radius
-      }
-    }).done(function (response) {
-      var i;
-      var marker;
-      var store;
-      var infoMarkup = [];
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          map.setCenter(pos);
+          searchCircle.setCenter(pos);
+      }, function() {
+        // Browser supports Geolocation but smth went wrong
+      });
+    } else {
+      // Browser doesn't support Geolocation
+    }
 
-      clearOverlays();
-
-      if (response.length !== 0) {
-        for (i = 0; i < response.length; i++) {
-          store = response[i];
-          infoMarkup.push('<p><strong>'
-            + store.promotion.company.substr(0, 20)
-            + '</strong></p><p><a href="promotions/'
-            + store.promotion.id
-            + '"><strong>'
-            + store.promotion.promotionname.substr(0, 30)
-            + '</strong></a></p><p>'
-            + store.promotion.promotiondesc.substr(0, 60)
-            + '</p>');
-          marker = new google.maps.Marker({
-            position: new google.maps.LatLng(store.location[0], store.location[1]),
-            map: map
-          });
-          markersArray.push(marker);
-          google.maps.event.addListener(marker, 'click', (function (marker, i) {
-            return function () {
-              infoWindow.setContent('<div>' + infoMarkup[i] + '</div>')
-              infoWindow.open(map, marker)
-            }
-          })(marker, i));
+    function getData(lat, lng, radius) {
+      $.ajax({
+        url: 'webapi/locations',
+        type: 'get',
+        dataType: 'json',
+        data: {
+          lat: lat,
+          lng: lng,
+          radius: radius
         }
-      }
-    });
-    function clearOverlays() {
-      for (var i = 0; i < markersArray.length; i++ ) {
-        markersArray[i].setMap(null);
-      }
-      markersArray.length = 0;
+      }).done(function (response) {
+        var i;
+        var marker;
+        var store;
+        var infoMarkup = [];
+
+        // Clearing overlays
+        for (var i = 0; i < markersArray.length; i++ ) {
+          markersArray[i].setMap(null);
+        }
+        markersArray.length = 0;
+
+        if (response.length !== 0) {
+          for (i = 0; i < response.length; i++) {
+            store = response[i];
+            infoMarkup.push('<p><strong>'
+              + store.promotion.company.substr(0, 20)
+              + '</strong></p><p><a href="promotions/'
+              + store.promotion.id
+              + '"><strong>'
+              + store.promotion.promotionname.substr(0, 30)
+              + '</strong></a></p><p>'
+              + store.promotion.promotiondesc.substr(0, 60)
+              + '</p>');
+            marker = new google.maps.Marker({
+              position: new google.maps.LatLng(store.location[0], store.location[1]),
+              map: map
+            });
+            markersArray.push(marker);
+            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+              return function () {
+                infoWindow.setContent('<div>' + infoMarkup[i] + '</div>')
+                infoWindow.open(map, marker)
+              }
+            })(marker, i));
+          }
+        }
+      });
     }
   }
-
-  // TODO: GEOLOCATION
-  // if (navigator.geolocation) {
-  //   navigator.geolocation.getCurrentPosition(function(position) {
-  //     var pos = {
-  //       lat: position.coords.latitude,
-  //       lng: position.coords.longitude
-  //     };
-  //     map.setCenter(pos);
-  //   }, function() {
-  //     // TODO: Browser supports Geolocation but smth went wrong
-  //   });
-  // } else {
-  //   // TODO: Browser doesn't support Geolocation
-  // }
-
-}
-
 </script>
