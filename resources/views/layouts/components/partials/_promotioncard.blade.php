ada29f2121f21dc393b0fca7fe99891cee13e58b
begimov <begimov@gmail.com>
2017-05-31 17:41:30 +0300

    equal height thumbnails cards for promo

--- resources/views/layouts/components/partials/_promotioncard.blade.php
+++ resources/views/layouts/components/partials/_promotioncard.blade.php
@@ -1,7 +1,7 @@
 @if (count($promotions) > 0)
   @foreach ($promotions as $promotion)
     <div class="col-sm-6 col-md-4">
-      <div class="thumbnail">
+      <div class="thumbnail equal-min-height">
         @if (count($promotion->images) > 0)
           <a href="{{ route('promotion.show', $promotion->id) }}">
             <img class="img-rounded" src="{{ $promotion->mediumImage->path }}" alt="{{ $promotion->promotionname }}">
