e32af2652bafa1bc978b51bebb2b092178a1b205
begimov <begimov@gmail.com>
2017-06-03 19:50:25 +0300

    location search validation and map output filtering by category added

--- app/Http/Controllers/Webapi/LocationController.php
+++ app/Http/Controllers/Webapi/LocationController.php
@@ -5,20 +5,31 @@ namespace App\Http\Controllers\Webapi;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Promotions\Location;
+use App\Http\Requests\Promotions\SearchLocationRequest;
 
 class LocationController extends Controller
 {
-  public function index(Request $request)
-  {
-      // TODO: Validation for lat and lng
-      $lat = $request->lat;
-      $lng = $request->lng;
-      $radius = $request->radius;
-      // \DB::enableQueryLog();
-      $locations = Location::distance($radius + $radius/(5/($radius/800)),"{$lat},{$lng}")->whereHas('promotion', function ($query) {
-          $query->where('active', 1);
-      })->with('promotion')->take(100)->get();
-      // dd(\DB::getQueryLog());
-      return $locations->toArray();
-  }
+    public function index(SearchLocationRequest $request)
+    {
+
+        $lat = $request->lat;
+        $lng = $request->lng;
+        $radius = $request->radius;
+        $category = $request->category;
+        $searchRadius = $radius + $radius/(5/($radius/800));
+        // \DB::enableQueryLog();
+        if ($category < 1) {
+            $locations = Location::distance($searchRadius,"{$lat},{$lng}")
+                ->whereHas('promotion', function ($query) use ($category){
+                    $query->where('active', 1);
+                })->with('promotion')->take(100)->get();
+        } else {
+            $locations = Location::distance($searchRadius,"{$lat},{$lng}")
+                ->whereHas('promotion', function ($query) use ($category){
+                    $query->where('active', 1)->where('category_id', $category);
+                })->with('promotion')->take(100)->get();
+        }
+        // dd(\DB::getQueryLog());
+        return $locations->toArray();
+    }
 }
--- resources/views/layouts/components/_map.blade.php
+++ resources/views/layouts/components/_map.blade.php
@@ -1,5 +1,19 @@
 <div class="row">
     <div class="col-md-12">
+      @if (count($categories) > 0)
+      <select name="category" id="category" class="form-control map-filter-category" onchange="filterMapOutput()">
+        <option value="0" selected="selected">Все категории</option>
+        @foreach ($categories as $category)
+        @unless (isset($category->parent_id))
+        <optgroup label="{{ $category->name }}">
+          @foreach ($category->subcategories as $subcategory)
+          <option value="{{ $subcategory->id }}">{{ $subcategory->name }}</option>
+          @endforeach
+        </optgroup>
+        @endunless
+        @endforeach
+      </select>
+      @endif
       <div class="map" id="map-canvas"></div>
     </div>
 </div>
@@ -10,11 +24,15 @@
 
 <script type="text/javascript">
 
+  var map;
+  var searchCircle;
+  var infoWindow;
+  var markersArray = [];
+
   function initMap() {
     var defaultCenterLat = 59.9307772;
     var defaultCenterLng = 30.3276762;
     var defaultSearchRadius = 5000;
-    var markersArray = [];
 
     var mapOptions = {
       zoom: 12,
@@ -34,11 +52,11 @@
       streetViewControl: false,
     }
 
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-    var infoWindow = new google.maps.InfoWindow();
+    infoWindow = new google.maps.InfoWindow();
 
-    var searchCircle = new google.maps.Circle({
+    searchCircle = new google.maps.Circle({
         strokeColor: '#2ea1a6',
         strokeOpacity: 0.8,
         strokeWeight: 2,
@@ -52,13 +70,13 @@
       });
 
     google.maps.event.addListener(searchCircle, 'radius_changed', function() {
-      getData(searchCircle.getCenter().lat(), searchCircle.getCenter().lng(), searchCircle.getRadius());
+      filterMapOutput();
     });
     google.maps.event.addListener(searchCircle, 'center_changed', function() {
-      getData(searchCircle.getCenter().lat(), searchCircle.getCenter().lng(), searchCircle.getRadius());
+      filterMapOutput();
     });
 
-    getData(defaultCenterLat, defaultCenterLng, defaultSearchRadius);
+    filterMapOutput();
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
@@ -74,57 +92,65 @@
     } else {
       // Browser doesn't support Geolocation
     }
+  }
 
-    function getData(lat, lng, radius) {
-      $.ajax({
-        url: 'webapi/locations',
-        type: 'get',
-        dataType: 'json',
-        data: {
-          lat: lat,
-          lng: lng,
-          radius: radius
-        }
-      }).done(function (response) {
-        var i;
-        var marker;
-        var store;
-        var infoMarkup = [];
+  function filterMapOutput() {
+    getData(searchCircle.getCenter().lat(),
+      searchCircle.getCenter().lng(),
+      searchCircle.getRadius(),
+      document.getElementById("category").value);
+  }
 
-        // Clearing overlays
-        for (var i = 0; i < markersArray.length; i++ ) {
-          markersArray[i].setMap(null);
-        }
-        markersArray.length = 0;
+  function getData(lat, lng, radius, category) {
+    $.ajax({
+      url: 'webapi/locations',
+      type: 'get',
+      dataType: 'json',
+      data: {
+        lat: lat,
+        lng: lng,
+        radius: radius,
+        category: category
+      }
+    }).done(function (response) {
+      var i;
+      var marker;
+      var store;
+      var infoMarkup = [];
 
-        if (response.length !== 0) {
-          for (i = 0; i < response.length; i++) {
-            store = response[i];
-            if (store.promotion) {
-              infoMarkup.push('<p><strong>'
-                + store.promotion.company.substr(0, 20)
-                + '</strong></p><p><a href="promotions/'
-                + store.promotion.id
-                + '"><strong>'
-                + store.promotion.promotionname.substr(0, 30)
-                + '</strong></a></p><p>'
-                + store.promotion.promotiondesc.substr(0, 60)
-                + '</p>');
-              marker = new google.maps.Marker({
-                position: new google.maps.LatLng(store.location[0], store.location[1]),
-                map: map
-              });
-              markersArray.push(marker);
-              google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                return function () {
-                  infoWindow.setContent('<div>' + infoMarkup[i] + '</div>')
-                  infoWindow.open(map, marker)
-                }
-              })(marker, i));
-            }
+      // Clearing overlays
+      for (var i = 0; i < markersArray.length; i++ ) {
+        markersArray[i].setMap(null);
+      }
+      markersArray.length = 0;
+
+      if (response.length !== 0) {
+        for (i = 0; i < response.length; i++) {
+          store = response[i];
+          if (store.promotion) {
+            infoMarkup.push('<p><strong>'
+              + store.promotion.company.substr(0, 20)
+              + '</strong></p><p><a href="promotions/'
+              + store.promotion.id
+              + '" target="_blank"><strong>'
+              + store.promotion.promotionname.substr(0, 30)
+              + '</strong></a></p><p>'
+              + store.promotion.promotiondesc.substr(0, 60)
+              + '</p>');
+            marker = new google.maps.Marker({
+              position: new google.maps.LatLng(store.location[0], store.location[1]),
+              map: map
+            });
+            markersArray.push(marker);
+            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+              return function () {
+                infoWindow.setContent('<div>' + infoMarkup[i] + '</div>')
+                infoWindow.open(map, marker)
+              }
+            })(marker, i));
           }
         }
-      });
-    }
+      }
+    });
   }
 </script>
