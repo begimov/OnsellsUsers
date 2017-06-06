4b06cf942640ef3b61f5d475fb956aafd8db2871
begimov <begimov@gmail.com>
2017-06-06 15:56:14 +0300

    loader animated gif moved

--- resources/views/layouts/components/_promotioncards.blade.php
+++ resources/views/layouts/components/_promotioncards.blade.php
@@ -3,7 +3,7 @@
 </div>
 <!-- <div>{{ $promotions->links() }}</div> -->
 <div class="ajax-load text-center" style="display:none">
-	<p><img src="http://demo.itsolutionstuff.com/plugin/loader.gif">Загрузка акций...</p>
+	<p><img src="{{ asset('img/promotions/loader.gif') }}">Загрузка акций...</p>
 </div>
 
 @section('postJquery')
