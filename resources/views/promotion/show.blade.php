511db2ca2a895ef098f85f901d25099ff9438bb7
begimov <begimov@gmail.com>
2017-08-04 12:04:12 +0300

    promotion page template changes depending on existing application from currently authed user

--- resources/views/promotion/show.blade.php
+++ resources/views/promotion/show.blade.php
@@ -22,34 +22,36 @@
       <div class="panel panel-default">
         <div class="panel-heading"><h4>{{ $promotion->company }}</h4></div>
         <div class="panel-body">
-          <p>
-            <strong>Адрес:</strong><br>
-            {{ $promotion->address }}
-          </p>
-          @isset($promotion->website)
+          @if($applied)
             <p>
-              <strong>Сайт:</strong><br>
-              <a href="{{ route('redirect.external', $promotion->id) }}">{{ $promotion->website }}</a>
+              <strong>Адрес:</strong><br>
+              {{ $promotion->address }}
             </p>
-          @endisset
-          <p>
-            <strong>Телефон:</strong><br>
-            {{ $promotion->phone }}
-          </p>
-          <p>
-            <hr>
-            @if (session('status'))
-              <div class="alert alert-success">
-                {{ session('status') }}
-              </div>
-            @else
-            @if (Auth::guest())
-              <a class="btn btn-primary" href="{{ route('application.create', $promotion->id) }}" role="button">Войти и получить скидку</a>
-            @else
-              <a class="btn btn-primary" href="{{ route('application.create', $promotion->id) }}" role="button">Получить скидку</a>
-            @endif
-            @endif
-          </p>
+            @isset($promotion->website)
+              <p>
+                <strong>Сайт:</strong><br>
+                <a href="{{ route('redirect.external', $promotion->id) }}">{{ $promotion->website }}</a>
+              </p>
+            @endisset
+            <p>
+              <strong>Телефон:</strong><br>
+              {{ $promotion->phone }}
+            </p>
+          @endif
+          @if (session('status'))
+            <div class="alert alert-success">
+              {{ session('status') }}
+            </div>
+          @endif
+          @if(!$applied)
+            <p>
+              @if (Auth::guest())
+                <a class="btn btn-primary" href="{{ route('application.create', $promotion->id) }}" role="button">Войти и получить скидку</a>
+              @else
+                <a class="btn btn-primary" href="{{ route('application.create', $promotion->id) }}" role="button">Получить скидку</a>
+              @endif
+            </p>
+          @endif
         </div>
         <div class="panel-footer">
           <div class="ya-share2"
