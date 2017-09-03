89224d87b99e861c7d61ac2a523a4fcd3dca0a70
begimov <begimov@gmail.com>
2017-09-03 12:16:44 +0300

    event and listener added for new application creation and sending related notifications

---
+++ app/Listeners/Promotions/SendApplicationCreatedNotification.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners\Promotions;
+
+use App\Events\Promotions\ApplicationCreated;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendApplicationCreatedNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  ApplicationCreated  $event
+     * @return void
+     */
+    public function handle(ApplicationCreated $event)
+    {
+        //
+    }
+}
