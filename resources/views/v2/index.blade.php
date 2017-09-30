8daca02b8281b8db6115b2d06f17bcf3b8ad40a6
begimov <begimov@gmail.com>
2017-09-30 07:08:06 +0300

    v2 index root component created

--- resources/views/v2/index.blade.php
+++ resources/views/v2/index.blade.php
@@ -1,1 +1,15 @@
+@extends('layouts.app')
 
+@section('titleandmetatags')
+  @include('layouts.partials._metatags')
+@endsection
+
+@section('content')
+
+  <div class="container">
+
+    <index></index>
+
+  </div>
+
+@endsection
