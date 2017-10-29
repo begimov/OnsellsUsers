c124f882628644ecfcd4428ab4a26a5dd0c0e269
begimov <begimov@gmail.com>
2017-10-29 07:06:44 +0300

    locations relationship eager loading added to webapi promotion controller index method

--- public/js/app.js
+++ public/js/app.js
@@ -16014,7 +16014,7 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
       }
     }
   }),
-  methods: _extends({}, __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0_vuex__["c" /* mapActions */])('promotions/catalog', ['switchActiveComponent', 'updateSearchQuery', 'getPromotions']), {
+  methods: _extends({}, __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0_vuex__["c" /* mapActions */])('promotions/catalog', ['updateSearchQuery', 'getPromotions']), {
     textSearch() {
       clearTimeout(this.timer);
       this.timer = setTimeout(function () {
