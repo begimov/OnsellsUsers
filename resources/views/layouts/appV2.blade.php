c2f4bb8d333e8b69cc774d54f53bbbe2c121ec53
begimov <begimov@gmail.com>
2018-02-18 12:06:25 +0300

    V2 version basic blade template created

---
+++ resources/views/layouts/appV2.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<!-- HEAD -->
+@include('layouts.partials._head')
+<body>
+  <div id="app">
+    <!-- NAVIGATION -->
+    @include('layouts.partials._navigation')
+    <!-- CONTENT -->
+    @yield('content')
+    <!-- FOOTER -->
+    @include('layouts.partials._footer')
+  </div>
+  <!-- Scripts -->
+  <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
+  <script src="{{ asset('js/app.js') }}"></script>
+  <script>
+      jQuery(document).ready(function() {
+          @yield('postJquery');
+      });
+  </script>
+  <!-- Additional SCRIPTS -->
+  @yield('scripts')
+</body>
+
+</html>
