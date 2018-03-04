d3011ab2290e9051ba72168d98feac76a4ff573e
begimov <begimov@gmail.com>
2018-03-04 08:30:34 +0300

    overall fine tunings made

--- resources/views/layouts/partialsV2/_navigation.blade.php
+++ resources/views/layouts/partialsV2/_navigation.blade.php
@@ -1,5 +1,5 @@
 <!-- NAVIGATION -->
-<nav class="navbar navbar-dark navbar-expand-lg" style="background-color: #150e16;">
+<nav class="navbar navbar-dark navbar-expand-md" style="background-color: #150e16;">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -15,22 +15,20 @@
                 <a class="nav-link" href="{{ route('login') }}">Войти</a>
             </li>
             @else
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                    {{ Auth::user()->name }} <span class="caret"></span>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {{ Auth::user()->name }}
                 </a>
-                <ul class="dropdown-menu" role="menu">
-                    <li>
-                        <a href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                        Выйти
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            Выйти
                         </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                           {{ csrf_field() }}
                         </form>
-                    </li>
-                </ul>
+                </div>
             </li>
             @endif
         </ul>
